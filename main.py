303cae84bb1ed849e88e02d1af0f31069c6a971a
mariomunozutn <mariomunozutn@gmail.com>
2026-04-28 14:35:25 -0300

    feature: implement menu with input validation

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.menu import show_main_menu
+
+# mostrar menu
+show_main_menu()
--- src/menu.py
+++ src/menu.py
@@ -1,3 +1,35 @@
 MENU = """
 | 1: Agregar país   | 2: Actualizar población | 3: Actualizar superficie | 4: Buscar país por nombre |
 | 5: Filtrar países | 6: Ordenar países       | 7: Mostrar estadísticas  | 8: Salir                  |"""
+MENU_ALLOW_OPTIONS = [1,2,3,4,5, 6,7,8]
+
+def show_main_menu() -> None:
+    # todas las opciones de menú estrán aqui dentro. Será la forma de volver al menú automaticamente luego de 
+    # ejecutar cada opcion del menu
+    while True:
+        print(f"\n {MENU}")
+        
+        menu_option = input("\nElija la opción del Menú: ")
+        # si es un número y está en en la lista de opciones permitidas
+        if not( menu_option.isdigit() and int(menu_option) in MENU_ALLOW_OPTIONS ):
+            print("\n[ERROR] - Ha elegido una opción que no se encuentra en el menú principal. Inténtelo nuevamente")
+            continue
+        
+        # Cada opción del menú se implementa en su propio módulo dentro de menu_actions/
+        # Convención: un archivo por opción, nombrado según la accion, ej: add_country.py
+        if int(menu_option) == 1:
+            None
+        elif int(menu_option) == 2:
+            None
+        elif int(menu_option) == 3:
+            None
+        elif int(menu_option) == 4:
+            None
+        elif int(menu_option) == 5:
+            None
+        elif int(menu_option) == 6:
+            None
+        elif int(menu_option) == 7:
+            None
+        elif int(menu_option) == 8:
+            None
