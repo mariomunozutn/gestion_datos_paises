156ab8a7bac1f049bdc621b21c82dde6f95da255
mariomunozutn <mariomunozutn@gmail.com>
2026-04-27 22:35:30 -0300

    chore: initial project structure

---
+++ src/menu.py
@@ -0,0 +1,3 @@
+MENU = """
+| 1: Agregar país   | 2: Actualizar población | 3: Actualizar superficie | 4: Buscar país por nombre |
+| 5: Filtrar países | 6: Ordenar países       | 7: Mostrar estadísticas  | 8: Salir                  |"""
